02b23469a3785cfd4181172509b596983f9e272e
Seroga <ilchuk.cergey@gmail.com>
2026-05-26 16:09:58 +0300

    Added scrapper functionality to Rick & Morty servece

--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+RICK_AND_MORTY_API_CHARACTERS_URL = "https://rickandmortyapi.com/api/character"
