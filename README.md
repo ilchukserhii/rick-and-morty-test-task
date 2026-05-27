fafa98e7c591bee16a8fe16f35f1d006e4cda428
Seroga <ilchuk.cergey@gmail.com>
2026-05-27 12:48:31 +0300

    Implement tests and implement Docker support

--- README.md
+++ README.md
@@ -2,14 +2,6 @@
 
 
 ## How tu run:
-- Create venv: `python -m venv .venv`
-- Activate: `source .venv/bin/acticate`
-- Install requirements: `pip install -r requirements.txt`
-- Create Postgres DB & User
 - Copy .evn.sample -> .env and populate with all required data
-- Run migrations: `python manage.py migrate`
-- Run Redis Server: `docker run -d -p 6379:6379 redis`
-- Run celery worker for task handling: `celery -A rick_and_morty_api worker -l INFO`
-- Run celery beat for task scheduling: `celery -A rick_and_morty_api beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler`
-- Create schedule for running sync in DB
-- Run app: `python manage.py runserver`
+- `docker compose up --build`
+- Create admin user & Create schedule for running sync in DB
--- characters/tests.py
+++ characters/tests.py
@@ -1,3 +1,65 @@
-from django.test import TestCase
+from unittest.mock import patch
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.urls import reverse
+
+from characters.models import Character
+from characters.serializers import CharacterSerializer
+
+
+class CharacterViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_character_filter_by_name(self):
+        character_1 = Character.objects.create(
+            api_id="1",
+            name="Character 1",
+            status="Alive",
+            species="Species 1",
+            gender="Male",
+            image="http://test.com/test.jpg"
+        )
+        character_2 = Character.objects.create(
+            api_id="2",
+            name="Character 2",
+            status="Alive",
+            species="Species 2",
+            gender="Male",
+            image="http://test.com/test.jpg"
+        )
+
+        response = self.client.get(
+            reverse("characters:character_list"),
+            data={"name": "Character 1"}
+        )
+        serializer_1 = CharacterSerializer(character_1)
+        serializer_2 = CharacterSerializer(character_2)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer_1.data, response.data)
+        self.assertNotEqual(serializer_2.data, response.data)
+
+    @patch("characters.views.random.choice")
+    def test_get_random_character(self, mock_choice):
+        character = Character.objects.create(
+            api_id="1",
+            name="Rick",
+            status="Alive",
+            species="Human",
+            gender="Male",
+            image="http://test.com/test.jpg"
+        )
+
+        mock_choice.return_value = character.pk
+
+        response = self.client.get(
+            reverse("characters:random_character")
+        )
+
+        serializer = CharacterSerializer(character)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
